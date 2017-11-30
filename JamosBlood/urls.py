187f5d76a164ddce9ef01b0f9791337f657c68a9
Michael Jolliffe <michael.a.jolliffe@gmail.com>
2017-11-30 11:23:18 -0800

    setting up URL routing and view for homepage

--- JamosBlood/urls.py
+++ JamosBlood/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from pages import views as page_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', page_views.home, name='home')
 ]
